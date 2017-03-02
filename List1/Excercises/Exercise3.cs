4293f1592015a21da6bc88fb9df2e32331b918d9
krystianolbrys <x11.crs.539@gmail.com>
2017-03-02 09:44:53 +0100

    Ex3 - 2b - Done

--- List1/Excercises/Exercise3.cs
+++ List1/Excercises/Exercise3.cs
@@ -6,11 +6,163 @@ using System.Threading.Tasks;
 
 namespace List1.Excercises
 {
-    class Exercise3
+    class Exercise3 //2b
     {
+        public static void initHelperTable(int[,] matrix, int maxOnes)
+        {
+            int i = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (i < maxOnes)
+                    {
+                        matrix[row, col] = 1;
+                        i++;
+                    }
+                    else
+                    {
+                        matrix[row, col] = 0;
+                    }
+                }
+            }
+        }
+
+        public static void initValuesTable(char[,] matrix)
+        {
+            int i = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = '-';
+                }
+            }
+        }
+
+        public static void populateValuesTable(int[,] helper, char[,] values, string word)
+        {
+            int i = 0;
+            for (int row = 0; row < helper.GetLength(0); row++)
+            {
+                for (int col = 0; col < helper.GetLength(1); col++)
+                {
+                    if (helper[row, col] == 1)
+                    {
+                        values[row, col] = word[i++];
+                    }
+                }
+            }
+        }
+
+        public static void printHelperTable(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void printValuesTable(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static string getEncrypted(int[,] helper, char[,] values, List<int> key)
+        {
+            string data = "";
+            foreach (int k in key)
+            {
+                for (int c = 0; c < helper.GetLength(0); c++)
+                {
+                    if (helper[c, k - 1] == 1) data += values[c, k - 1];
+                }
+                //data += " ";
+            }
+            return data;
+        }
+
+        public static string getDecrypted(int[,] helper, char[,] values, List<int> key, string wordEncrypted)
+        {
+            string data = "";
+            int counter = 0;
+
+            foreach (int k in key)
+            {
+                for (int c = 0; c < helper.GetLength(0); c++)
+                {
+                    if (helper[c, k - 1] == 1) values[c, k - 1] = wordEncrypted[counter++];
+                }
+                //data += " ";
+            }
+
+            //for (int rowTemp = 0; rowTemp < values.GetLength(0); rowTemp++)
+            //{
+            //    foreach (int k in key)
+            //    {
+            //        if (helper[rowTemp, k - 1] == 1)
+            //        {
+            //            values[rowTemp, k - 1] = wordEncrypted[counter++];
+            //        }
+            //    }
+            //}
+
+            for (int row = 0; row < values.GetLength(0); row++)
+            {
+                for (int col = 0; col < values.GetLength(1); col++)
+                {
+                    if (helper[row, col] == 1)
+                    {
+                        data += values[row, col];
+                    }
+                }
+            }
+
+            return data;
+        }
+
+        public static List<int> getKeyInversed(List<int> key)
+        {
+            List<int> lista = new List<int>();
+            int[] keyInverse = new int[key.Count];
+
+            int forLoopCounter = 1;
+            foreach (int k in key)
+            {
+                keyInverse[k - 1] = forLoopCounter;
+                forLoopCounter++;
+            }
+            key.Clear();
+            foreach (int VARIABLE in keyInverse)
+            {
+                lista.Add(VARIABLE);
+            }
+
+            return lista;
+        }
+
         public static void Encrypt()
         {
+            Console.Clear();
+            ConsoleOutput.PutInitText();
+            Console.WriteLine("Zadanie 5 - Vigenere");
+            Console.WriteLine();
 
+            Console.Write("Podaj ciąg znaków do szyfrowania: ");
+            var plaintext = Console.ReadLine();
+            Console.Write("Podaj klucz: ");
+            var keyInput = Console.ReadLine();
         }
 
         public static void Decrypt()
--- List1/Excercises/Exercise5.cs
+++ List1/Excercises/Exercise5.cs
@@ -8,19 +8,167 @@ namespace List1.Excercises
 {
     class Exercise5
     {
+        public static char[] getAlfabetTable()
+        {
+            List<char> alpbabetList = new List<char>();
+
+            for (int i = 65; i < 91; i++)
+            {
+                alpbabetList.Add((char)i);
+            }
+
+            char[] alfabet = alpbabetList.ToArray();
+
+            return alfabet;
+        }
+
+        public static int getShift(char c, char[] alfabet)
+        {
+            for (int i = 0; i < alfabet.Length; i++)
+            {
+                if (alfabet[i] == c) return i;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public static char getCharEncodeCaesar(char c, int shift, char[] alfabet)
+        {
+            return alfabet[(getShift(c, alfabet) + shift) % alfabet.Length];
+        }
+
+        public static char getCharDecodeCaesar(char c, int shift, char[] alfabet)
+        {
+            int x = (getShift(c, alfabet) - shift) % alfabet.Length;
+
+            if (x < 0) x = x + alfabet.Length;
+
+            return alfabet[x];
+        }
+
+        public static string getStringEncodeCaesar(string input, int shift, char[] alfabet)
+        {
+            input = input.ToUpper();
+            string output = "";
+
+            foreach (char c in input)
+            {
+                output += getCharEncodeCaesar(c, shift, alfabet);
+            }
+
+            return output;
+        }
+
+        public static string getStringDecodeCaesar(string input, int shift, char[] alfabet)
+        {
+            input = input.ToUpper();
+            string output = "";
+
+            foreach (char c in input)
+            {
+                output += getCharDecodeCaesar(c, shift, alfabet);
+            }
+
+            return output;
+        }
+
+        //Vigenere
+
+        public static int getPositionAlphabet(char c, char[] alphabet)
+        {
+            int counter = 0;
+            foreach (char VARIABLE in alphabet)
+            {
+                if (VARIABLE == c) return counter;
+                counter++;
+            }
+            throw new NotImplementedException();
+        }
+
+        public static int[] getKeyTable(string input, char[] alphabet)
+        {
+            int[] table = new int[input.Length];
+
+            int counter = 0;
+            foreach (char VARIABLE in input)
+            {
+                table[counter++] = getPositionAlphabet(VARIABLE, alphabet);
+            }
+
+            return table;
+        }
+
+        //Main :)
+
         public static void Encrypt()
         {
-            
+            Console.Clear();
+            ConsoleOutput.PutInitText();
+            Console.WriteLine("Zadanie 5 - Vigenere");
+            Console.WriteLine();
+
+            Console.Write("Podaj ciąg znaków do szyfrowania: ");
+            var plaintext = Console.ReadLine();
+            Console.Write("Podaj klucz: ");
+            var keyInput = Console.ReadLine();
+
+            char[] Alfabet = getAlfabetTable();
+
+            string word = plaintext.ToUpper();
+            string wordEncoded = "";
+
+            int[] key = getKeyTable(keyInput.ToUpper(), Alfabet);
+
+            Console.WriteLine();
+
+            int keyCounter = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (keyCounter == key.Length) keyCounter = 0;
+                //Console.Write(getCharEncodeCaesar(word[i],key[keyCounter++],Alfabet));
+                wordEncoded += getCharEncodeCaesar(word[i], key[keyCounter++], Alfabet);
+            }
+            Console.WriteLine("Ciąg zaszyfrowany: " + wordEncoded);
+
+            Console.ReadKey();
         }
 
         public static void Decrypt()
         {
+            Console.Clear();
+            ConsoleOutput.PutInitText();
+            Console.WriteLine("Zadanie 5 - Vigenere");
+            Console.WriteLine();
 
+            Console.Write("Podaj ciąg znaków do deszyfrowania: ");
+            var plaintext = Console.ReadLine();
+            Console.Write("Podaj klucz: ");
+            var keyInput = Console.ReadLine();
+
+            char[] Alfabet = getAlfabetTable();
+
+            string word = plaintext.ToUpper();
+            string wordDecoded = "";
+
+            int[] key = getKeyTable(keyInput.ToUpper(), Alfabet);
+
+            Console.WriteLine();
+
+            int keyCounter = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (keyCounter == key.Length) keyCounter = 0;
+                //Console.Write(getCharEncodeCaesar(word[i],key[keyCounter++],Alfabet));
+                wordDecoded += getCharDecodeCaesar(word[i], key[keyCounter++], Alfabet);
+            }
+            Console.WriteLine("Ciąg odszyfrowany: " + wordDecoded);
+
+            Console.ReadKey();
         }
 
-        private static string GetAlphabet()
-        {
-            return "ABCDEFGHIJKLMNOPRSTUVWXYZ";
-        }
+        //private static string GetAlphabet()
+        //{
+        //    return "ABCDEFGHIJKLMNOPRSTUVWXYZ";
+        //}
     }
 }
