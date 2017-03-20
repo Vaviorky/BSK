4f9de00ac0eff26ecad62ae0c6a1d327f4ea58fa
krystianolbrys <x11.crs.539@gmail.com>
2017-03-20 15:14:36 +0100

    Lista2 Zad2

---
+++ List2/Exercises/ClassZad2/LFSR.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List2.Exercises.ClassZad2
+{
+    class LFSR
+    {
+        private int[] dictioanryTab;
+        private byte[] seedTab;
+
+        public LFSR(string dict, string seed)
+        {
+            dictioanryTab = getWielomian(dict);
+            seedTab = getSeddTab(seed);
+        }
+
+        public byte getLFSR_Value()
+        {
+            byte XOR = getLFSR_Byte(dictioanryTab, seedTab);
+            seedTab = getShiftedSeed(seedTab, XOR);
+            return XOR;
+        }
+
+        private int[] getWielomian(string s)
+        {
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '1') count++;
+            }
+
+            int[] wielomianInts = new int[count];
+            count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '1') wielomianInts[count++] = i;
+            }
+
+            return wielomianInts;
+        }
+
+        private byte[] getSeddTab(string s)
+        {
+            byte[] tab = new byte[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '1') tab[i] = 1;
+                else tab[i] = 0;
+            }
+            return tab;
+        }
+
+        private byte getLFSR_Byte(int[] dict, byte[] seed)
+        {
+            byte XOR = getXOR(seed[dict[0]], seed[dict[1]]);
+
+            for (int i = 2; i < dict.Length; i++)
+            {
+                XOR = getXOR(XOR, seed[dict[i]]);
+            }
+
+            return XOR;
+        }
+
+        public byte getXOR(byte a, byte b)
+        {
+            return (byte)(a ^ b);
+
+            //if (a == 0 && b == 0) return 0;
+            //if (a == 0 && b == 1) return 1;
+            //if (a == 1 && b == 0) return 1;
+            //if (a == 1 && b == 1) return 0;
+
+            //return 0;
+        }
+
+        private byte[] getShiftedSeed(byte[] seed, byte XOR)
+        {
+            byte[] newSedd = new byte[seed.Length];
+            for (int i = 0; i < (seed.Length - 1); i++)
+            {
+                newSedd[i + 1] = seed[i];
+            }
+            newSedd[0] = XOR;
+
+            return newSedd;
+        }
+    }
+}
--- List2/Exercises/Exercise2.cs
+++ List2/Exercises/Exercise2.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using List2.Exercises.ClassZad2;
 
 namespace List2.Exercises
 {
@@ -11,7 +12,31 @@ namespace List2.Exercises
             Console.WriteLine("Zadanie 2 - Synchronous Stream Cipher - Szyfrowanie");
             Console.WriteLine();
 
+            string dictWord;
+            string seed;
 
+            string input, output;
+
+            Console.Write("Podaj wielomian np. 1001: ");
+            dictWord = Console.ReadLine();
+
+            Console.Write("Podaj seed np. 1010: ");
+            seed = Console.ReadLine();
+
+            Console.Write("Podaj input file: ");
+            input = Console.ReadLine();
+
+            Console.Write("Podaj output file: ");
+            output = Console.ReadLine();
+
+
+            LFSR o = new LFSR(dictWord, seed);
+            //input = "C:/biomet/output.bin";
+            //input = "C:/biomet/Lenna1.png";
+            //output = "C:/biomet/fromBin.png";
+
+            LFSR_2_File zad2 = new LFSR_2_File(o, input, output);
+            zad2.DoWork();
         }
 
         public static void Decrypt()
